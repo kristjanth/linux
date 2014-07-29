209311dcc2cd14be192091749d4e53719c7cafe3
Kristjan Thorvaldsson <kristjan@kt.is>
2014-07-29 12:58:06 +0000

    Update kt-useradd.sh

--- kt-useradd.sh
+++ kt-useradd.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 useradd -m "$1"
 echo "$1:$2" | chpasswd
-su - "$1" -c "echo $2 > ~/sudo-pass"
-su - "$1" -c "mkdir ~/.ssh"
 su - "$1" -c "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa"
 su - "$1" -c "rm -f ~/.ssh/id_rsa*"
 su - "$1" -c "echo $3 > ~/.ssh/authorized_keys"
