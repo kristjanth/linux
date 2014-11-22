d8b4ffca01e4d147a82cef4b5caddaf490333de8
Kristjan Thorvaldsson <kristjan@kt.is>
2014-11-22 16:52:50 +0000

    Delete kt-useradd.sh

--- kt-useradd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-useradd -m "$1"
-echo "$1:$2" | chpasswd
-su - "$1" -c "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa"
-su - "$1" -c "rm -f ~/.ssh/id_rsa*"
-su - "$1" -c "echo $3 > ~/.ssh/authorized_keys"
-su - "$1" -c "chmod 0600 ~/.ssh/authorized_keys"
