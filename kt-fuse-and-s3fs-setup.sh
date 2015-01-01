80ade793cc0c0ab2353448de3418c61608f34e8a
Kristjan Thorvaldsson <kristjan@kt.is>
2015-01-01 21:51:56 +0000

    Update kt-fuse-and-s3fs-setup.sh

--- kt-fuse-and-s3fs-setup.sh
+++ kt-fuse-and-s3fs-setup.sh
@@ -12,27 +12,25 @@
 # = 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
-AWS_ACCESS_KEY_ID="AAAAAAAAAAAAAA"
-AWS_SECRET_ACCESS_KEY="XXXXXXXXXXXXXXXXXXXXXXX"
+AWS_ACCESS_KEY_ID="AA"
+AWS_SECRET_ACCESS_KEY="XX"
 AWS_S3BUCKET="bucket"
 S3_MOUNT_PATH="/mnt/s3"
-S3_CACHE_PATH="/tmp/s3cache"
 FUSE_URL="http://downloads.sourceforge.net/project/fuse/fuse-2.X/2.9.3/fuse-2.9.3.tar.gz"
 FUSE_PATH="/usr/src/fuse"
-S3FS_URL="https://s3fs.googlecode.com/files/s3fs-1.74.tar.gz"
+S3FS_URL="https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.78.tar.gz"
 S3FS_PATH="/usr/src/s3fs"
 
 # Do not change anything below this
 
 yum remove -y -q fuse fuse-s3fs &> /dev/null
-yum install -y -q gcc libstdc++-devel gcc-c++ curl-devel libxml2-devel openssl-devel mailcap &> /dev/null
+yum install -y -q gcc libstdc++-devel gcc-c++ curl-devel libxml2-devel openssl-devel mailcap automake &> /dev/null
 
 wget -q -O /tmp/fuse.tar.gz $FUSE_URL &> /dev/null
 tar -xzf /tmp/fuse.tar.gz -C /tmp &> /dev/null
 rm -f /tmp/fuse.tar.gz &> /dev/null
 rm -rf /usr/src/fuse &> /dev/null
 mv /tmp/fuse* $FUSE_PATH &> /dev/null
-
 cd $FUSE_PATH
 ./configure --prefix=/usr/local &> /dev/null
 make &> /dev/null
@@ -46,8 +44,8 @@ tar -xzf /tmp/s3fs.tar.gz -C /tmp &> /dev/null
 rm -f /tmp/s3fs.tar.gz &> /dev/null
 rm -rf /usr/src/s3fs &> /dev/null
 mv /tmp/s3fs* $S3FS_PATH &> /dev/null
-
 cd $S3FS_PATH
+./autogen.sh &> /dev/null
 ./configure --prefix=/usr/local &> /dev/null
 make &> /dev/null
 make install &> /dev/null
@@ -65,5 +63,5 @@ mkdir $S3_CACHE_PATH &> /dev/null
 mkdir $S3_MOUNT_PATH &> /dev/null
 chmod 777 $S3_CACHE_PATH $S3_MOUNT_PATH &> /dev/null
 
-s3fs -o use_cache=$S3_CACHE_PATH $AWS_S3BUCKET $S3_MOUNT_PATH &> /dev/null
+s3fs $AWS_S3BUCKET $S3_MOUNT_PATH &> /dev/null
 
