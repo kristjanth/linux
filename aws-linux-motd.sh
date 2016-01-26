a3e1026563c8d0ace9fb1afe44640caba6430742
Kristjan Thorvaldsson <kristjan@kt.is>
2016-01-26 13:37:54 +0000

    Too support longer instance ID format in AWS

--- aws-linux-motd.sh
+++ aws-linux-motd.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+ID=${ID:2}
 LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
 cat > /etc/motd <<EOF
 --------------------------------------------------------------------------------
-$ZONE - $ID
+$ZONE
+$ID
 $PUBLIC_IP
 $LOCAL_IP
 --------------------------------------------------------------------------------
