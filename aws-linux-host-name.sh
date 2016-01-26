2a8e92810868ec6b392fddc42d3dc2208abb254d
Kristjan Thorvaldsson <kristjan@kt.is>
2016-01-26 13:38:16 +0000

    Too support longer instance ID format in AWS

--- aws-linux-host-name.sh
+++ aws-linux-host-name.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-hostname $ZONE"-"$ID
+ID=${ID:2}
+hostname $ID
