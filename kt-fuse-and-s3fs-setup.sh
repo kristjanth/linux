9132573d8d6a0e4c6b3f5c0387b31308545dda71
Kristjan Thorvaldsson <kristjan@kt.is>
2020-07-08 12:40:08 +0000

    Update kt-fuse-and-s3fs-setup.sh
    Updated libfuse and s3fs url

--- kt-fuse-and-s3fs-setup.sh
+++ kt-fuse-and-s3fs-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-AWS_ACCESS_KEY_ID="AAAAAAAA"
-AWS_SECRET_ACCESS_KEY="XXXXXXXXXXXXXXXXXX"
-AWS_S3BUCKET="mybucketname"
+AWS_ACCESS_KEY_ID="ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY="SECRET_ACCESS_KEY"
+AWS_S3BUCKET="S3BUCKET"
 S3_MOUNT_PATH="/mnt/s3"
-FUSE_URL="https://github.com/libfuse/libfuse/archive/fuse-3.9.2.tar.gz"
+FUSE_URL="https://github.com/libfuse/libfuse/releases/download/fuse-3.0.0/fuse-3.0.0.tar.gz"
 FUSE_PATH="/usr/src/fuse"
-S3FS_URL="https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.86.tar.gz"
+S3FS_URL="https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.81.tar.gz"
 S3FS_PATH="/usr/src/s3fs"
 
 # Do not change anything below this
@@ -18,8 +18,8 @@ tar -xzf /tmp/fuse.tar.gz -C /tmp &> /dev/null
 rm -f /tmp/fuse.tar.gz &> /dev/null
 rm -rf /usr/src/fuse &> /dev/null
 mv /tmp/libfuse-fuse* $FUSE_PATH &> /dev/null
-cd $FUSE_PATH
-,&> /dev/null
+cd $FUSE_PATH &> /dev/null
+./configure --prefix=/usr/local &> /dev/null
 make &> /dev/null
 make install &> /dev/null
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig &> /dev/null
@@ -31,7 +31,7 @@ tar -xzf /tmp/s3fs.tar.gz -C /tmp &> /dev/null
 rm -f /tmp/s3fs.tar.gz &> /dev/null
 rm -rf /usr/src/s3fs &> /dev/null
 mv /tmp/s3fs-fuse* $S3FS_PATH &> /dev/null
-cd $S3FS_PATH
+cd $S3FS_PATH &> /dev/null
 ./autogen.sh &> /dev/null
 ./configure --prefix=/usr/local &> /dev/null
 make &> /dev/null
